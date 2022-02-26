58396bc3294b466de5b0296d118edc67a741a9b2
CharlesbrownK <ekjh0418@gmail.com>
2022-02-26 22:43:00 +0900

    Add sql save function

--- bugobot.py
+++ bugobot.py
@@ -14,19 +14,28 @@
 #   along with this program. If not, see <http://www.gnu.org/licenses/>
 
 import time
+import sqlite3
 import discord
 import asyncio
 from socket import timeout
 from codes.api import lunch_api
 from discord.ext import commands
 
+# make user_data database
+conn = sqlite3.connect("user.db")
+cur = conn.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS usersData(guild_name TEXT, guild_owner TEXT, grade TEXT, class_number TEXT);")
+
+# bot initial settings
 prefix = '?'
 blm = lunch_api.BugoLunchMenu()
-bot = commands.Bot(command_prefix = prefix)
+intents= discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix = prefix, intents=intents)
 tomorrow_lunch = blm.tm_lunch()
 today_lunch = blm.td_lunch()
 
-
+# bot status
 @bot.event
 async def on_ready():
     print('로그인중입니다. ')
@@ -34,44 +43,13 @@ async def on_ready():
     print('연결이 완료되었습니다.')
     await bot.change_presence(status=discord.Status.online, activity=discord.Game('BugoBot 일'))
 
-@bot.command()
-async def 급식(ctx):
-    if (len(today_lunch) == 0):
-        await ctx.send('''```\n오늘은 급식이 없습니다.[또는 데이터를 불러오지 못했습니다]\n```''')
-    else:
-        m = len(today_lunch)
-        i = 0
-        cnt = 1
-        menu_message = "```"
-        while (m >= 0):
-            menu_message += cnt + ". " + today_lunch[i] + "\n"
-            cnt += 1
-            i += 1
-            m -= 1
-        menu_message += '```'
-        await ctx.send(menu_message)
 
-@bot.command()
-async def 내일급식(ctx):
-    if (len(tomorrow_lunch) == 0):
-        await ctx.send('''```\n내일은 급식이 없습니다.[또는 데이터를 불러오지 못했습니다]\n```''')
-    else:
-        m = len(tomorrow_lunch)
-        i = 0
-        cnt = 1
-        menu_message = "```"
-        while (m >= 0):
-            menu_message += cnt + ". " + tomorrow_lunch[i] + "\n"
-            cnt += 1
-            i += 1
-            m -= 1
-        menu_message += '```'
-        await ctx.send(menu_message)
-        
-@bot.command(name="시간표")
-async def time_table(ctx):
+@bot.command(name="설정")
+async def settings(ctx):
     timeout = 7
-    await ctx.send("""```시간표를 확인하기 전에 사용자의 반 정보를 알아야 합니다!\n\n몇 반인지 알려주세요!\n   ex) 2-4 [⚠️ 해당 형식을 꼭 지켜주세요]\n```""")
+    await ctx.send("기본 세팅 모드입니다.")
+    text = """```\n몇 반인지 알려주세요!\n   ex) 2-4 [⚠️ 해당 형식을 꼭 지켜주세요]\n```"""
+    await ctx.send(text)
     time.sleep(0.5)
     send_message = await ctx.send(f'{timeout}초간 기다립니다!')
 
@@ -84,14 +62,33 @@ async def time_table(ctx):
         await ctx.send("""```\n시간이 초과되었네요!\n다시 명령어를 입력해주세요!\n```""")
     else:
         users_class = str(msg.content)
-        global user_class
         user_class = users_class[2:]
-        
-        global user_grade
         user_grade = users_class[0]
+        
+        guild_name = str(ctx.guild.name)
+        guild_owner = str(bot.get_user(int(ctx.guild.owner.id)))
+        
+        cur.execute('INSERT INTO usersData VALUES(?, ?, ?, ?);', (guild_name, guild_owner, user_grade, user_class))
+        
+        await ctx.send("사용자의 정보입니다.")
+        await ctx.send(f"""```\n{user_grade}학년 {user_class}반\n```""")
+        await ctx.send("만약 올바르지 않다면, 명령어를 다시 입력해주세요!")
+
+
+@bot.command(name="시간표")
+async def time_table(ctx):
+    await ctx.send(f"오늘 ??학년 ??반 시간표 입니다.")
+    await ctx.send("""```\n1. \n2. \n3. \n4. \n```""")
+
+
+@bot.command(name="디비정보")
+async def db_info(ctx):
+    cur.execute("SELECT * FROM usersData")
+    conn.commit()
+    rows = cur.fetchall()
     
-        await ctx.send(f"오늘 {user_grade}학년 {user_class}반 시간표 입니다.")
-        await ctx.send("""```\n1. \n2. \n3. \n4. \n```""")
+    for row in rows:
+        print(row)
 
 # @bot.command()
 # async def 오늘시간표(ctx):
@@ -102,3 +99,6 @@ async def time_table(ctx):
     
 
 bot.run('your_secret_token')
+
+cur.close()
+conn.close()
